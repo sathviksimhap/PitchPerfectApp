d4c1937dce1576651b1450a1fb8bc23c8c32b67a
sathviksimhap <sathviksimhap@gmail.com>
2025-02-19 17:51:12 +0530

    first

---
+++ PitchPerfectApp v1/functions.py
@@ -0,0 +1,67 @@
+from data import *
+from random import *
+from scamp import *
+
+def get_rand_key():
+    global notes_list
+    return choice(notes_list)
+
+def get_scale(key=get_rand_key(), mode = "easy"):
+    global notes_freq
+
+    start_scale = randint(0,1)
+    note = notes_freq[key][start_scale]
+
+    playable_notes = [note]
+    for i in range(modes[mode]):
+        for i in range(2):
+            note+=2
+            playable_notes.append(note)
+        note += 1
+        playable_notes.append(note)
+        for i in range(3):
+            note+=2
+            playable_notes.append(note)
+        note += 1
+        playable_notes.append(note)
+
+    new_scale = {
+        "key": key,
+        "octave": start_scale+1,
+        "notes": playable_notes
+    }
+
+    return new_scale
+
+def gen_question(scale, length = 3):
+    notes = scale["notes"]
+    questions = choices(notes, k = length)
+
+    answers = []
+    for note in questions:
+        answers.append(notes_list[note % 12])
+
+    question = {
+        "notes": questions,
+        "names": answers
+    }
+
+    return question
+
+def play_question(question):
+    notes = question["notes"]
+
+    s = Session()
+    piano = s.new_part("piano")
+
+    for i in range(3):
+        piano.play_note(60, 0, 1)
+        for note in notes:
+            piano.play_note(note, 1, 1)
+
+def create_button_clicked():
+    pass
+
+def play_button_clicked():
+    pass
+
---
+++ PitchPerfectApp v1/main.py
@@ -0,0 +1,56 @@
+from scamp import *
+from data import *
+from functions import *
+from tkinter import *
+
+BG_YELLOW = "#f7f5dd"
+
+scale = get_scale()
+question = gen_question(scale, 3)
+
+# while True:
+#     n = input()
+#     if n == "0":
+#         break
+#     if n == "2":
+#         print(question["names"])
+#     else:
+#         play_question(question)
+
+############################################UI##################################
+
+# Window
+window = Tk()
+window.title("PitchPerfect")
+window.minsize(width=1920, height=1080)
+window.config(padx=30, pady=30, bg=BG_YELLOW)
+
+# Title label
+title_label = Label(text="Pitch-Perfect",fg="#6495ED", font=("Showcard Gothic", 64))
+title_label.config(padx=30, pady=30, bg=BG_YELLOW)
+title_label.grid(row=0, column=2, columnspan=3)
+
+#Notes label
+notes_string = ""
+notes_label = Label(text="Notes:"+notes_string, font=("Magneto", 30))
+notes_label.config(padx=30, pady=30, bg=BG_YELLOW)
+notes_label.grid(row=1, column=0, columnspan=2)
+
+# Scale label
+scale_string = ""
+scale_label = Label(text="Scale:"+scale_string, font=("Magneto", 30))
+scale_label.config(padx=30, pady=30, bg=BG_YELLOW)
+scale_label.grid(row=3, column=0, columnspan=2)
+
+#Create button
+create_button = Button(text="Create", font=("Magneto", 30), command=create_button_clicked)
+create_button.config(padx=30, pady=30, bg=BG_YELLOW)
+create_button.grid(row=1, column=4)
+
+#Play button
+play_button = Button(text="Play", font=("Magneto", 30), command=play_button_clicked)
+play_button.config(padx=30, pady=30, bg=BG_YELLOW)
+play_button.grid(row=1, column=4)
+
+
+window.mainloop()
